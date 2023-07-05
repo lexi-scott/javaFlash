55a64c28a22d7915240973669ff9cfc57bb4c5da
slexicon <lexiscott0101@gmail.com>
2023-07-05 11:42:19 -0700

    front end

--- src/components/flashcard.jsx
+++ src/components/flashcard.jsx
@@ -11,8 +11,27 @@ function Cards() {
     <>
 
       <View style={{
-        flexDirection: 'column'
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
       }}>
+        {/* cancel button */}
+        <TouchableOpacity
+          style={{
+            padding: 20,
+            marginTop: 200
+          }}
+        >
+          <Icon
+            name='times-circle-o'
+            type='FontAwesome'
+            onPress={() => console.log("arrow clicked")}
+            style={{
+              fontSize: 30,
+              color: '#880808'
+            }} />
+        </TouchableOpacity>
+
+        {/* card front */}
         <FlipCard
           friction={6}
           perspective={10000}
@@ -21,12 +40,13 @@ function Cards() {
         >
           <View
             style={{
-              marginTop: 120,
               borderWidth: 3,
               borderRadius: 5,
               width: 200,
               height: 250,
-              textAlign: 'center'
+              textAlign: 'center',
+              top: '50%',
+              left: '50%',
             }}
           >
             <Text h3
@@ -44,54 +64,42 @@ function Cards() {
               hello
             </Text>
           </View>
-          <Text
+
+          {/* card back */}
+          <View
             style={{
               borderWidth: 3,
               borderRadius: 5,
               width: 200,
               height: 250,
               textAlign: 'center',
-              paddingTop: 20,
-            }}
-          >
-            Here is answer
-          </Text>
-        </FlipCard>
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between'
-        }}>
-          <TouchableOpacity
-            style={{
-              padding: 20,
-            }}
-          >
-            <Icon
-              name='times-circle-o'
-              type='FontAwesome'
-              onPress={() => console.log("arrow clicked")}
-              style={{
-                fontSize: 30,
-                color: '#880808'
-              }} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              padding: 20,
-            }}
-          >
-            <Icon
-              name='check-circle-o'
-              type='FontAwesome'
-              onPress={() => console.log("arrow clicked")}
-              style={{
-                fontSize: 30,
-                color: 'green'
-              }} />
-          </TouchableOpacity>
-        </View>
-      </View>
+              top: '50%',
+              left: '50%',
+            }}>
+            <Text>
+              Here is answer BACK CARD MORE ANSWER HERE A LONG ANSWER
+            </Text>
+          </View>
 
+        </FlipCard>
+
+        {/* Correct Button */}
+        <TouchableOpacity
+          style={{
+            padding: 20,
+            marginTop: 200
+          }}
+        >
+          <Icon
+            name='check-circle-o'
+            type='FontAwesome'
+            onPress={() => console.log("arrow clicked")}
+            style={{
+              fontSize: 30,
+              color: 'green'
+            }} />
+        </TouchableOpacity>
+      </View>
     </>
   );
 }
